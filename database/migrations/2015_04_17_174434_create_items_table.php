eaa4c30244b4e977828697ded65c730bc02132d5
Jules Rogmans <websitewizardguy@gmail.com>
2015-04-17 11:20:42 -0700

    Add items migration

---
+++ database/migrations/2015_04_17_174434_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('description');
+			$table->date('needed_by');
+			$table->integer('priority');
+			$table->integer('contact_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('items');
+	}
+
+}
