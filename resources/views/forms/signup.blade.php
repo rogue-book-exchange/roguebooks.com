63d2cf5dd13496f0ef7e0252d5dc548c639ac734
Ted Hendricks <trumpetted88@gmail.com>
2015-05-27 20:20:20 +0000

    Finish front-end signup form

--- resources/views/forms/signup.blade.php
+++ resources/views/forms/signup.blade.php
@@ -11,8 +11,8 @@
     {!! Form::text('first_name', null, ['placeholder' => 'First Name']) !!}
     {!! Form::text('last_name', null, ['placeholder' => 'Last Name']) !!}
     {!! Form::text('email', null, ['placeholder' => 'Email']) !!}
-    {!! Form::text('home_phone', null, ['placeholder' => 'Home Phone']) !!}
-    {!! Form::text('cell_phone', null, ['placeholder' => 'Cell Phone']) !!}
+    {!! Form::text('primary_phone', null, ['placeholder' => 'Primary Phone']) !!}
+    {!! Form::text('secondary_phone', null, ['placeholder' => 'Secondary Phone']) !!}
     {!! Form::text('address', null, ['placeholder' => 'Address']) !!}
     {!! Form::text('city', null, ['placeholder' => 'City']) !!}
     {!! Form::text('zip', null, ['placeholder' => 'Zip Code']) !!}
@@ -26,26 +26,29 @@
   <br>
   <h4>AVAILABILITY FOR SERVICE</h4>
   <p>I am available to work beginning:</p>
-    {!! Form::input('date', 'start_date', null, ['class' => 'form-control']) !!}
+    {!! Form::input('start_date', 'start_date', null, ['class' => 'form-control']) !!}
   <p><br>Please indicate your preferences for days and hours to volunteer:</p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Wednesday&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Thursday&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Friday&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Saturday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'sunday') !!}&nbsp;Sunday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'monday') !!}&nbsp;Monday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'tuesday') !!}&nbsp;Tuesday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'wednesday') !!}&nbsp;Wednesday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'thursday') !!}&nbsp;Thursday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'friday') !!}&nbsp;Friday&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('weekday_group[]', 'saturday') !!}&nbsp;Saturday&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br>Volunteering:</p>
   <p>
-    {!! Form::radio('agree') !!}&nbsp;Long-term&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::radio('agree') !!}&nbsp;Temporary(state number of weeks)&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::text('weeks') !!}
+    {!! Form::radio('duration', 'Long Term') !!}&nbsp;Long-term&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('duration', 'Temporary') !!}&nbsp;Temporary(state number of weeks if known)&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::text('number_weeks') !!}
   </p>
   <p><br>How often can you work?</p>
   <p>
-    {!! Form::radio('agree') !!}&nbsp;More frequently than weekly&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::radio('agree') !!}&nbsp;Weekly&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::radio('agree') !!}&nbsp;Biweekly&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::radio('agree') !!}&nbsp;Monthly&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('frequency', 'Weekly Plus') !!}&nbsp;More frequently than weekly&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('frequency', 'Weekly') !!}&nbsp;Weekly&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('frequency', 'Biweekly') !!}&nbsp;Biweekly&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('frequency', 'Monthly') !!}&nbsp;Monthly&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <br>
   <h4>PHYSICAL & SENSORY DEMANDS</h4>
@@ -58,62 +61,62 @@
   <p>(Note: all assignments will be made according to ability as determined by management)</p>
   <p><strong>Book Processing:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Straighten Shelves&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Sort Books&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Shelve Books&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_processing_group[]', 'straigten_shelves') !!}&nbsp;Straighten Shelves&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_processing_group[]', 'sort_books') !!}&nbsp;Sort Books&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_processing_group[]', 'shelve_books') !!}&nbsp;Shelve Books&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br><strong>Research & Analysis:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Scanning&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Listing Online&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('research_analysis_group[]', 'scanning') !!}&nbsp;Scanning&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('research_analysis_group[]', 'listing_online') !!}&nbsp;Listing Online&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br><strong>Front Desk Reception:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Customer Service&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Phone&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('front_desk_group[]', 'customer_service') !!}&nbsp;Customer Service&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('front_desk_group[]', 'answer_phone') !!}&nbsp;Answer Phone&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br><strong>Light Janitorial:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Dust & Vacuum&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Clean Bathrooms&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Recycling&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('janitorial_group[]', 'dust_vacuum') !!}&nbsp;Dust & Vacuum&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('janitorial_group[]', 'clean_bathroom') !!}&nbsp;Clean Bathroom&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('janitorial_group[]', 'recycling') !!}&nbsp;Recycling&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br><strong>Special Projects:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Fund raising events&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Offsite Booth&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Committees&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('special_projects_group[]', 'fund_raising') !!}&nbsp;Fund raising events&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('special_projects_group[]', 'offsite_booth') !!}&nbsp;Offsite Booth&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('special_projects_group[]', 'committees') !!}&nbsp;Committees&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br><strong>Book Pickups:</strong></p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Medford&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Central Point&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Eagle Point&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Shady Cove&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Ashland&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Talent/Phoenix&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Jacksonville/Applegate/Ruch&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'medford') !!}&nbsp;Medford&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'central_point') !!}&nbsp;Central Point&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'eagle_point') !!}&nbsp;Eagle Point&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'shady_cove') !!}&nbsp;Shady Cove&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'ashland') !!}&nbsp;Ashland&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'talent_phoenix') !!}&nbsp;Talent/Phoenix&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('book_pickups_group[]', 'jacksonville_applegate_ruch') !!}&nbsp;Jacksonville/Applegate/Ruch&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p>I have an SUV, pickup truck or van that I'm willing to use:</p>
   <p>
-    {!! Form::radio('agree') !!}&nbsp;yes&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::radio('agree') !!}&nbsp;no&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('has_vehicle') !!}&nbsp;yes&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::radio('has_vehicle') !!}&nbsp;no&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <br>
   <p><br><strong>Tell us about yourself:</strong></p>
   <p>I'm interested in:</p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Non-fiction&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;General Fiction&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Mystery&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Romance&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Westerns&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Science Fiction/Fantasy&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'non_fiction') !!}&nbsp;Non-fiction&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'general_fiction') !!}&nbsp;General Fiction&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'mystery') !!}&nbsp;Mystery&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'romance') !!}&nbsp;Romance&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'westerns') !!}&nbsp;Westerns&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'science_fiction') !!}&nbsp;Science Fiction/Fantasy&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p>
-    {!! Form::checkbox('agree') !!}&nbsp;Religious/Spirituality&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Teens/Tweens&nbsp;&nbsp;&nbsp;&nbsp;
-    {!! Form::checkbox('agree') !!}&nbsp;Children's&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'religous') !!}&nbsp;Religious/Spirituality&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'teens') !!}&nbsp;Teens/Tweens&nbsp;&nbsp;&nbsp;&nbsp;
+    {!! Form::checkbox('personal_interests_group[]', 'childrens') !!}&nbsp;Children's&nbsp;&nbsp;&nbsp;&nbsp;
   </p>
   <p><br>My computer skills include:</p>
     {!! Form::textarea('computer_skills', null, ['size' => '150x3']) !!}
