debe5ddc61ea1bb5fd29880e494fca9fbb99a896
Jules Rogmans <websitewizardguy@gmail.com>
2015-06-01 14:12:40 -0700

    Add admin view

---
+++ resources/views/pages/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+
+@section('title')
+  Admin Settings
+@stop
+
+@section('content')
+  <div class="row">
+    <div class="col-sm-6">
+      <h3>User Name: </h3>
+    </div>
+  </div>
+@stop
+
+@section('footer')
+  @include('partials.footer')
+@stop
