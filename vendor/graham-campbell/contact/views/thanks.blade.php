52d696d60d971baa9190f2094b1c7971a425abf7
Jules Rogmans <websitewizardguy@gmail.com>
2015-06-01 20:30:39 -0700

    Remove vendor/ from git

--- vendor/graham-campbell/contact/views/thanks.blade.php
+++
@@ -1,8 +0,0 @@
-@extends(Config::get('core.email'))
-
-@section('content')
-<p>Thank you {{ $name }} for contacting <a href="{{ $url }}">{{ $platform}}</a>.</p>
-<p>Your message will be reviewed, and you may be contacted again via this email address if required.</p>
-<p>For reference, your message was:<p>
-<blockquote>{!! $quote !!}</blockquote>
-@stop
